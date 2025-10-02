c6fb0eb23cdc55568c066831d80a4620274757bf
asifikbal09 <asif.ikbal662@gmail.com>
2025-10-02 23:16:42 +0600

    [DONE] Initial Layout setup.

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Layout, Menu } from 'antd';
+const { Header, Content, Sider } = Layout;
+
+const items = [
+    {
+        key: '1',
+        label: 'Option 1',
+    },
+    {
+        key: '2',
+        label: 'Option 2',
+        children:[
+            {
+                key: '2-1',
+                label: 'Option 2-1',
+            },
+            {
+                key: '2-2',
+                label: 'Option 2-2',
+            }
+        ]
+    }
+]
+
+const MainLayout = () => {
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+          
+        }}
+      >
+        <div
+        className="demo-logo-vertical"
+        style={{
+          color: "white",
+          fontWeight: "bold",
+          height: "4rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1>PlayTrack</h1>
+        </div>
+        <Menu theme='dark' mode='inline' defaultSelectedKeys={['4']} items={items} />
+      </Sider>
+      <Layout>
+        <Header />
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+
+            }}
+          >
+            content
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  )
+};
+export default MainLayout;
